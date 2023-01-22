796545ebc60569cd192765db73d94207c96437cc
abhigyachhabra <abhigya.chhabra@mail.utoronto.ca>
2023-01-21 23:28:45 -0500

    Adding Itinerary containers

---
+++ trav_rec/src/ItenaryCont.js
@@ -0,0 +1,20 @@
+import './ItenaryCont.css';
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+class ItenaryCont extends React.Component{
+    constructor(props){
+        super(props);
+
+    }
+
+render(){
+    return(
+        <div className="MainCont">
+            This is the main cont
+        </div>
+    )
+}
+}
+
+export default ItenaryCont
